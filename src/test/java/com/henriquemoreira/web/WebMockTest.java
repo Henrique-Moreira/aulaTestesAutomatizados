e982513c83b35e1fa311806b4205d9e4b8d300ab
Henrique-Moreira <henriquemoreiraa@gmail.com>
2021-04-24 22:59:30 -0300

    Adição MockMVC testing web

--- src/test/java/com/henriquemoreira/web/WebMockTest.java
+++ src/test/java/com/henriquemoreira/web/WebMockTest.java
@@ -1,29 +1,41 @@
 package com.henriquemoreira.web;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import com.henriquemoreira.controllers.HomeController;
+import com.henriquemoreira.controllers.GreetingController;
+import com.henriquemoreira.services.GreetingService;
 
-@WebMvcTest(HomeController.class)
-public class WebLayerTest {
-
+@WebMvcTest(GreetingController.class)
+public class WebMockTest {
+  
+	//para permitir que as requisições sejam feitas sem a necessidade de instanciar um servidor(tomcat)
 	@Autowired
 	private MockMvc mockMvc;
 
+	//cria um obj alternativo a dependencia do service, simula o comportamento do service
+	@MockBean
+	private GreetingService service;
+
 	@Test
-	public void shouldReturnDefaultMessage() throws Exception {
-		this.mockMvc.perform(get("/"))
+	public void greetingShouldReturnMessageFromService() throws Exception {
+		
+		when(service.greet()).thenReturn("Hello, Mock");
+		
+		this.mockMvc.perform(get("/greeting"))
 		.andDo(print())
 		.andExpect(status().isOk())
-		.andExpect(content().string(containsString("Hello, World")));
-	}
+		.andExpect(content().string(containsString("Hello, Mock")));
+	} 
 }
